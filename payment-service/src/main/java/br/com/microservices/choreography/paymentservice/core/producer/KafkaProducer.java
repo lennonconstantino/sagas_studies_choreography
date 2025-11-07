25dd207918223a99dc02f30298c07b346e1d3ca4
lennonconstantino <lennonconstantino@gmail.com>
2025-11-06 23:03:57 -0300

    fix producers and consumers

--- payment-service/src/main/java/br/com/microservices/choreography/paymentservice/core/producer/KafkaProducer.java
+++ payment-service/src/main/java/br/com/microservices/choreography/paymentservice/core/producer/KafkaProducer.java
@@ -2,7 +2,6 @@ package br.com.microservices.choreography.paymentservice.core.producer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -13,15 +12,12 @@ public class KafkaProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @Value("${spring.kafka.topic.orchestrator}")
-    private String orchestratorTopic;
-
-    public void sendEvent(String payload) {
+    public void sendEvent(String payload, String topic) {
         try {
-            log.info("Sending event to topic {} with data {}", orchestratorTopic, payload);
-            kafkaTemplate.send(orchestratorTopic, payload);
+            log.info("Sending event to topic {} with data {}", topic, payload);
+            kafkaTemplate.send(topic, payload);
         } catch (Exception ex) {
-            log.error("Error trying to send data to topic {} with data {}", orchestratorTopic, payload, ex);
+            log.error("Error trying to send data to topic {} with data {}", topic, payload, ex);
         }
     }
 }
